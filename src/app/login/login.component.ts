49617e6932583ef33adc19e2acfb3fcd17b205a7
zainab <mahmoudzainab040@gmail.com>
2024-02-02 22:04:05 +0200

    added Log In

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,47 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule,RouterLinkActive,RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+
+ gameForm: FormGroup;
+  constructor() {
+    this.gameForm = new FormGroup({
+       fname: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+        
+      ]), 
+      lname: new FormControl('', [
+       Validators.required,
+       Validators.minLength(3),
+     ]),
+     email: new FormControl('', [
+       Validators.required,
+       Validators.pattern(/^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/)
+        ]),
+     password: new FormControl('', [
+       Validators.required,
+       Validators.minLength(8),
+     ]),
+     birthday: new FormControl('', [
+       Validators.required,
+       Validators.pattern(/^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/),
+     ])
+   });
+ }
+ handelForm() {
+
+   console.log(this.gameForm);
+ }
+
 
 }
