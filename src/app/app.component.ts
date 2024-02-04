0224919cc79cdb5fb1bfc2cb31f6aa7e08ec730d
mohamedlink <mohamedlink1999@gmail.com>
2024-02-04 23:13:54 +0200

    include nav and fotter for all compp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
+import { NavbarComponent } from './navbar/navbar.component';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NavbarComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
