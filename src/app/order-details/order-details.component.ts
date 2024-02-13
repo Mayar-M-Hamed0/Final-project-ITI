6ee6892647842a627c765b494dbf4b014604ff44
zainab <mahmoudzainab040@gmail.com>
2024-02-13 10:25:04 +0200

    Order Details adding

---
+++ src/app/order-details/order-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './order-details.component.html',
+  styleUrl: './order-details.component.css'
+})
+export class OrderDetailsComponent {
+
+}
