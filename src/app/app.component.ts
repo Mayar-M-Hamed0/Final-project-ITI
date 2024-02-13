3b74b5c1226152bed191f54647d1f03ff23d3711
Anan Mohsen Mahmoud <ananelemery@gmail.com>
2024-02-13 17:58:30 +0200

    serviceform

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ServiceformComponent } from './serviceform/serviceform.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet , ServiceformComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/sing-up/sing-up.component.ts
+++ src/app/sing-up/sing-up.component.ts
@@ -40,5 +40,4 @@ export class SingUpComponent { gameForm: FormGroup;
    console.log(this.gameForm);
  }
 
-
 }
