644de81502b8843e8acf940c88d520268a3a9c0e
Abdelrahman shooieb <abdoshoieb93@gmai.com>
2024-02-14 14:58:07 +0200

    i added the rating stars and comment field

---
+++ src/app/rating-stars/rating-stars.component.ts
@@ -0,0 +1,19 @@
+import { CommonModule } from '@angular/common';
+import { Component,Input } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-rating-stars',
+  standalone: true,
+  imports: [FontAwesomeModule,CommonModule],
+  templateUrl: './rating-stars.component.html',
+  styleUrl: './rating-stars.component.css'
+})
+export class RatingStarsComponent {
+  faStar = faStar;
+
+
+  @Input() actualRating:number = 0;
+
+}
