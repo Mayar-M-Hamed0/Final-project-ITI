0f06f3be0edf7bf87bf738bb5b6a6102bdfd2f8a
Mayar-M-Hamed0 <mayar.m.hamed97@gmail.com>
2024-03-04 16:55:45 +0200

    filter

---
+++ src/app/filter-modal/filter-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filter-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './filter-modal.component.html',
+  styleUrl: './filter-modal.component.css'
+})
+export class FilterModalComponent {
+
+}
