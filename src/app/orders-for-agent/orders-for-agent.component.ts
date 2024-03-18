1de39bc1fe403bf4ae184c910f21ed085318464d
Mayar-M-Hamed0 <mayar.m.hamed97@gmail.com>
2024-03-18 23:27:22 +0200

    pagination

---
+++ src/app/orders-for-agent/orders-for-agent.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-orders-for-agent',
+  standalone: true,
+  imports: [],
+  templateUrl: './orders-for-agent.component.html',
+  styleUrl: './orders-for-agent.component.css'
+})
+export class OrdersForAgentComponent {
+
+}
